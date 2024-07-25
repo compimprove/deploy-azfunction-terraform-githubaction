179dd0be651f5cb9a63a1ef4adeed161553e48e2
DinhNT <compimprove@gmail.com>
2024-07-25 10:58:16 +0700

    fix the sequence

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,6 +57,6 @@ resource "azurerm_linux_function_app" "simpleapp_app" {
       node_version = 20
     }
   }
-  lifecycle { ignore_changes = [app_settings["WEBSITE_RUN_FROM_PACKAGE"]] }
+  lifecycle { ignore_changes = [app_settings["WEBSITE_RUN_FROM_PACKAGE"], app_settings["WEBSITE_ENABLE_SYNC_UPDATE_SITE"]] }
 }
 
